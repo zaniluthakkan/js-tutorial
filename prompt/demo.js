22198b42906cc074f52a7b80473d0a2baeb3a3bd
zaniluthakkan <92579943+zaniluthakkan@users.noreply.github.com>
2021-11-24 09:42:17 +0530

    Add files via upload

---
+++ prompt/demo.js
@@ -0,0 +1,9 @@
+function sum_of_numbers(){
+    var a = parseInt(prompt("Enter the first number"));
+    var b = parseInt(prompt("Enter the second number"));
+    sum = a + b;
+    return sum;
+    
+}
+alert(sum_of_numbers());
+    
